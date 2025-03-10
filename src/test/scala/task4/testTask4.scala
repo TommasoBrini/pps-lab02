d9dba73205b0cf91dffcd5167481743f149b1591
Tommaso <tommaso.brini@studio.unibo.it>
2025-03-10 16:45:39 +0100

    ex9: testShowLiteral failed

--- src/test/scala/task4/testTask4.scala
+++ src/test/scala/task4/testTask4.scala
@@ -10,15 +10,25 @@ class testTask4:
 
   @Test def testEvaluateLiteral(): Unit = {
     val literal = Literal(4)
-    assertEquals(evaluate(literal), 4)
+    assertEquals(4, evaluate(literal))
   }
 
   @Test def testEvaluateAdd(): Unit = {
     val add = Add(Add(Literal(4), Literal(3)),Literal(3))
-    assertEquals(evaluate(add), 10)
+    assertEquals(10, evaluate(add))
   }
 
   @Test def testEvaluateMultiply(): Unit = {
     val multiply = Multiply(Multiply(Literal(2), Literal(5)), Literal(4))
-    assertEquals(evaluate(multiply), 40)
+    assertEquals(40, evaluate(multiply))
+  }
+
+  @Test def testEvaluateCombined(): Unit = {
+    val result = Multiply(Add(Literal(4), Literal(6)), Literal(4))
+    assertEquals(40, evaluate(result))
+  }
+
+  @Test def testShowLiteral(): Unit = {
+    val literal = Literal(5)
+    assertEquals("5", show(literal))
   }
