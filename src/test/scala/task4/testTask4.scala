db28575773894fc3667f65494ef3ffc00eda9ff1
Tommaso <tommaso.brini@studio.unibo.it>
2025-03-10 16:39:26 +0100

    ex9: testEvaluateMultiply failed

--- src/test/scala/task4/testTask4.scala
+++ src/test/scala/task4/testTask4.scala
@@ -9,11 +9,16 @@ import task4.Exercise9.Expr.*
 class testTask4:
 
   @Test def testEvaluateLiteral(): Unit = {
-    val literal = Expr.Literal(4)
+    val literal = Literal(4)
     assertEquals(evaluate(literal), 4)
   }
 
   @Test def testEvaluateAdd(): Unit = {
-    val add = Expr.Add(Add(Literal(4), Literal(3)),Literal(3))
+    val add = Add(Add(Literal(4), Literal(3)),Literal(3))
     assertEquals(evaluate(add), 10)
+  }
+
+  @Test def testEvaluateMultiply(): Unit = {
+    val multiply = Multiply(Multiply(Literal(2), Literal(5)), Literal(4))
+    assertEquals(evaluate(multiply), 40)
   }
