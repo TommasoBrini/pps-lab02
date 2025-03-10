2ca96093b24327cc6eb3ecb72cbd4ff290e75e31
Tommaso <tommaso.brini@studio.unibo.it>
2025-03-10 13:42:50 +0100

    solved ex4

--- src/main/scala/task2/Exercise4.scala
+++ src/main/scala/task2/Exercise4.scala
@@ -1,5 +1,19 @@
 package task2
 
 object Exercise4 extends App:
-    // TODO
-    ???
+
+    // A
+    val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y == z
+    println(p1(1)(3)(3))
+
+    // B
+    val p2: (Int, Int, Int) => Boolean = (x, y, z) => x <= y && y == z
+    println(p2(1,3,3))
+
+    // C
+    def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && y == z
+    println(p3(1)(3)(3))
+
+    // D
+    def p4(x: Int, y: Int, z: Int): Boolean = x <= y && y == z
+    println(p4(1,3,3))
