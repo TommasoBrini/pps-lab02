cb19a6328e7a399e68fad230c33c0d2e55e3bc2c
Tommaso <tommaso.brini@studio.unibo.it>
2025-03-10 16:01:12 +0100

    ex9: testEvaluateAdd checked

--- src/test/scala/task4/testTask4.scala
+++ src/test/scala/task4/testTask4.scala
@@ -8,7 +8,13 @@ import task4.Exercise9.Expr.*
 
 class testTask4:
 
-  @Test def testLiteral(): Unit = {
+  @Test def testEvaluateLiteral(): Unit = {
     val literal = Expr.Literal(4)
     assertEquals(evaluate(literal), 4)
+  }
+
+  @Test def testEvaluateAdd(): Unit = {
+    val add = Expr.Add(3,4)
+    assertEquals(evaluate(add, 7))
+
   }
