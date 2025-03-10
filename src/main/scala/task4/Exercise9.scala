20b05af5e4a99bc01d5cd7c7391c1b4881d3e5f1
Tommaso <tommaso.brini@studio.unibo.it>
2025-03-10 16:40:33 +0100

    ex9: testEvaluateMultiply checked

--- src/main/scala/task4/Exercise9.scala
+++ src/main/scala/task4/Exercise9.scala
@@ -5,10 +5,11 @@ object Exercise9 extends App:
   enum Expr:
     case Literal(i: Int)
     case Add(a: Expr, b: Expr)
-    case Multiply
+    case Multiply(a: Expr, b: Expr)
 
   object Expr:
 
     def evaluate(expr: Expr): Int = expr match
       case Literal(i) => i
       case Add(a, b) => evaluate(a) + evaluate(b)
+      case Multiply(a, b) => evaluate(a) * evaluate(b)
